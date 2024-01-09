f96d9403b4488ec9a6fa3506aee8f9137635cc20
monika8848 <monika@8848digital.com>
2024-01-09 18:16:29 +0530

    feat: show only common object keys inputfield in UI

--- src/app/Components/InputFields/InputFields.tsx
+++ src/app/Components/InputFields/InputFields.tsx
@@ -3,45 +3,50 @@ import ProductProcessAPI from "../../Dataset/ProductProcessAPI";
 import OperationCardDetail from "../../Dataset/OperationCardDetail";
 
 const InputFields = () => {
-  const [productFieldData, setProductFieldData] = useState<any>([]);
-
   // Product process Data
   // finding objects key from objects and it will return those keys in array
-  const objectKeyArrProduct = Object.keys(ProductProcessAPI);
-  console.log(objectKeyArrProduct, "objectKeyArrProduct");
+  //   const objectKeyArrProduct = Object.keys(ProductProcessAPI);
 
-  const objectValueArrProduct = Object.values(ProductProcessAPI);
-  console.log("objectValueArrProduct", objectValueArrProduct);
+  //   const objectValueArrProduct = Object.values(ProductProcessAPI);
+
+  const filteredValues: any = Object.entries(ProductProcessAPI)
+    .filter(([key, value]) => key.startsWith("show") && value === 1)
+    .map(([key, value]) => key.replace("show_", ""));
 
   //   Mapping over to get value which start from show and then after remove that show
-  const objectWithShowKey = objectKeyArrProduct.map((keyData: any) => {
-    if (keyData?.startsWith("show")) {
-      const filtered = keyData.replace("show_", "");
-      return filtered;
-    }
-    return null;
-  });
+  //   const objectWithShowKey = objectKeyArrProduct.map((keyData: any) => {
+  //     if (keyData?.startsWith("show")) {
+  //       const filtered = keyData.replace("show_", "");
+  //       return filtered;
+  //     }
+  //     return null;
+  //   });
 
   //   Detail
   // finding objects key from objects and it will return those keys in array of detail
   const objectKeyArrDetail = Object.keys(OperationCardDetail);
 
-  const commonData = objectWithShowKey.filter((data) => {
-    return <p>{objectKeyArrDetail.includes(data)}</p>;
-  });
+  //   const commonData = objectWithShowKey.filter((data) => {
+  //     return <p>{objectKeyArrDetail.includes(data)}</p>;
+  //   });
+  const commonData = filteredValues.filter((data: any) =>
+    objectKeyArrDetail.includes(data)
+  );
 
   return (
     <div>
       {/* {objectValueArrProduct.map((ele: any) => {
         return <div>{ele}</div>;
       })} */}
-
+      {/* {filteredValues.map((ele: any) => {
+        return <div>{ele}</div>;
+      })} */}
       {commonData.length > 0 &&
         commonData.map((data: any, index: any) => {
           return (
             <div>
               <label>{data}</label>
-              {data?.length > 0 && <input type="text" />}
+              <input type="text" />
             </div>
           );
         })}
